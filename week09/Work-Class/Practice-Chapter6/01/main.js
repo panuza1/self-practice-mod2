7c5dee063c026c022328bc80e0b813fd94df0b39
Panupong Chaichun <panuponghaichun@gmail.com>
2025-10-09 01:12:18 +0700

    w9_fix

--- week09/Work-Class/Practice-Chapter6/01/main.js
+++ week09/Work-Class/Practice-Chapter6/01/main.js
@@ -1,12 +1,21 @@
-const username = document.getElementById('username')
-const email = document.getElementById('email')
-const password = document.getElementById('password')
-const confirm_password = document.getElementById('confirm-password')
-const btn = document.getElementById('submit')
-const text_action = document.querySelector("form>p")
+const btn_create = document.getElementById("submit")
+const username = document.getElementById("username") 
+const email = document.getElementById("email") 
+const password = document.getElementById("password")
+const confirm_password = document.getElementById("confirm-password") 
+const text = document.querySelector("p")
 
-btn.addEventListener("submit", (event) => {
-    e.preventDefault(); 
-})
-
-
+btn_create.addEventListener("click", (e) => {
+    e.preventDefault()    
+    if (!username.value || !email.value || !password.value || !confirm_password.value) {
+        text.textContent = "missing some value, please by again!"
+        text.style.color = "red "
+    } else if (password.value !== confirm_password.value){
+        text.textContent = "password and comfirm do not match, check again"
+        text.style.color = "red"
+    } else {
+        console.log(username.value)
+        text.textContent = "your data completed"
+        text.style.color = "green"
+    }
+}) 
